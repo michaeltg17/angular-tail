2726405f14f59ebe4e754b7b5db732fe57150c4c
Michael Trullas <adoser2@hotmail.es>
2026-01-24 17:53:15 +0100

    WIP

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,42 @@ import { Component, signal } from '@angular/core';
 import { CustomersTable } from './components/customers-table/customers-table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [CustomersTable, MatToolbarModule, MatButtonModule],
+  imports: [CustomersTable, MatToolbarModule, MatButtonModule, MatSelectModule, MatFormFieldModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('app');
+  palettes = [
+    'red',
+    'green',
+    'blue',
+    'yellow',
+    'cyan',
+    'magenta',
+    'orange',
+    'chartreuse',
+    'spring-green',
+    'azure',
+    'violet',
+    'rose',
+  ];
+  selectedPalette = this.palettes[1];
+
+  ngOnInit() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) this.selectedPalette = savedTheme;
+    document.documentElement.className = this.selectedPalette;
+  }
+
+  setTheme(theme: string) {
+    this.selectedPalette = theme;
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }
 }
