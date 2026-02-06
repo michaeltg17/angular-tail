a004a85233af4b95031cecf769a9228b4a3790e6
Michael Trullas <adoser2@hotmail.es>
2026-02-06 13:49:55 +0100

    first test ok

--- e2e/app-title.spec.ts
+++ e2e/app-title.spec.ts
@@ -3,4 +3,7 @@ import { test, expect } from '@playwright/test';
 test('has correct title', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle('Angular App');
+  // toolbar should contain the app title
+  const toolbar = page.locator('mat-toolbar span').first();
+  await expect(toolbar).toHaveText('Angular App');
 });
